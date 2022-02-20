64d2bad3a8fe96966783753f6710521eca0a75eb
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-02-20 23:15:48 +0530

    today work

---
+++ dom/dom.js
@@ -0,0 +1,11 @@
+// ###############Dom################
+// const mainheading =document.querySelector('#main-heading');
+// console.log(mainheading.textContent)
+
+// mainheading.textContent = 'this is the end';
+// console.log(mainheading.textContent)
+
+
+const mainheading = document.querySelector('div.headline h2');
+console.log(mainheading);
+mainheading.style.backgroundColor = 'blue';
