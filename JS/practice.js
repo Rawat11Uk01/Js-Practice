bb08ab893704259d5ad0680bef3a32d2af2c08e3
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-05 09:29:03 +0530

    today work

--- JS/practice.js
+++ JS/practice.js
@@ -538,3 +538,28 @@
 // const tommy = new Dog("tommy", 3);
 // console.log(tommy);
 // console.log(tommy.isCute());
+
+
+// function returning promise
+
+function ricePromise(){
+    const bucket = ['coffee', 'chips','vegetables','salts','rice'];
+    return new Promise((resolve,reject)=>{
+      if(bucket.includes("vegetables")&& bucket.includes("salt") && bucket.includes("rice")){
+          resolve({value:"friedrice"});
+      }else{
+          reject("could not do it");
+      }
+    })
+  }
+  
+  
+  ricePromise().then(
+    // jab promise resolve hoga 
+    (myfriedRice)=>{
+    console.log("lets eat ", myfriedRice);
+    }
+    ).catch(
+    (error)=>{
+        console.log(error)
+    })
