72df40842e877ea66dce2dfc42c841981e1c1eb6
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-06 08:34:26 +0530

    today work

--- Strings=Practice/strings-intro.js
+++ Strings=Practice/strings-intro.js
@@ -1,7 +1,21 @@
 // / string concatenation 
 
-let string1 = "17";
-let string2 = "10";
+// let string1 = "17";
+// let string2 = "10";
 
-let newString = +string1 + +string2;
-console.log(typeof newString);
+// let newString = +string1 + +string2;
+// console.log(typeof newString);
+
+
+
+
+// template string 
+// let age = 22;
+// let firstName = "kashish"
+
+// "my name is kashish and my age is 22 "
+// let aboutMe = "my name is " + firstName + " and my age is " + age; 
+
+// let aboutMe = `my name is ${firstName} and my age is ${age}`
+
+// console.log(aboutMe);
