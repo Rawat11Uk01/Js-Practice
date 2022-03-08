6dad312af6c32867c7bf00e964af99511681d658
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-09 02:26:19 +0530

    today work

--- JS/practice.js
+++ JS/practice.js
@@ -645,12 +645,46 @@
 //     })
   
 
-var age = 17;
-if(age >= 18){
-  console.log("you are eligible to vote");
-}else{
-  console.log("you are not eligible to vote");
-}
+// var age = 17;
+// if(age >= 18){
+//   console.log("you are eligible to vote");
+// }else{
+//   console.log("you are not eligible to vote");
+// }
 
-var age = 18;
-console.log((age >= 18) ? "you can vote" : "you can't vote");
+// var age = 18;
+// console.log((age >= 18) ? "you can vote" : "you can't vote");
+
+
+// var area = "square" ;
+// var PI = 3.142, l=5, b=4, r=3;
+
+// if(area == "circle"){
+//   console.log("the area of the circle is : " + PI*r**2);
+// }else if(area == "triangle"){
+//   console.log("the area of the triangle is : " + (l*b)/2);
+// }else if(area == "rectangle"){
+//   console.log("the area of the rectangle is : " + (l*b));
+// }else{
+//   console.log("please enter valid data");
+// }
+
+// var area = "dsfsad" ;
+// var PI = 3.142, l=5, b=4, r=3;
+
+// switch(area){
+//   case 'circle': 
+//     console.log("the area of the circle is : " + PI*r**2);
+//     break;
+
+//   case 'triangle':
+//     console.log("the area of the triangle is : " + (l*b)/2);
+//     break;
+
+//   case 'rectangle':
+//     console.log("the area of the rectangle is : " + (l*b));
+//     break;
+
+//   default:
+//     console.log("please enter valid data");
+// }   
