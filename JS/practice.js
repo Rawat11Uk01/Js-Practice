411be1e3f851234f0b571e76fa71c78a90e52ab1
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-11 07:05:14 +0530

    today work

--- JS/practice.js
+++ JS/practice.js
@@ -711,3 +711,23 @@ const arr = [
 // })
 
 console.log(arr.flat(Infinity));
+
+// const add = (a,b) => {
+//     return a+b;
+// }
+// // console.log(add(5,2));
+
+// const subs = (a,b) => {
+//     return Math.abs(a-b);
+// }
+// const mult = (a,b) => {
+//     return a*b;
+// }
+
+// const calculator = (num1,num2, operator) => {
+//   return operator(num1,num2);
+// }
+
+// calculator(5,2,subs)
+
+// console.log(calculator(5,2,subs));
