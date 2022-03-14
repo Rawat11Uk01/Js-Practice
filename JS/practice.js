bbfc12f5444eec83ccf1434ba182f02d11f43b5a
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-03-14 09:00:27 +0530

    first commit

--- JS/practice.js
+++ JS/practice.js
@@ -773,31 +773,44 @@ const heading4 = document.querySelector(".heading4");
 const heading5 = document.querySelector(".heading5");
 const heading6 = document.querySelector(".heading6");
 const heading7 = document.querySelector(".heading7");
-setTimeout(() => {
-  heading1.textContent = "one";
-  heading1.style.color = "violet";
-  setTimeout(() => {
-    heading2.textContent = "two";
-    heading2.style.color = "red";
-    setTimeout(() => {
-      heading3.textContent = "three";
-      heading3.style.color = "purple";
-      setTimeout(() => {
-        heading4.textContent = "four";
-        heading4.style.color = "pink";
-        setTimeout(() => {
-          heading5.textContent = "five";
-          heading5.style.color = "green";
-          setTimeout(() => {
-            heading6.textContent = "six";
-            heading6.style.color = "blue";
-            setTimeout(() => {
-              heading7.textContent = "seven";
-              heading7.style.color = "brown";
-            }, 1000);
-          }, 3000);
-        }, 2000);
-      }, 1000);
-    }, 2000);
-  }, 2000);
-}, 1000);
+
+function changeText(elem, text, color, time){
+    if(elem){
+        setTimeout(()=>{
+            elem.textContent = text;
+            elem.style.color  = color;
+            },time);
+    }else{
+        console.log('element not found')
+    }
+
+}
+changeText(heading1,'one','violet',1000);
+// setTimeout(() => {
+//   heading1.textContent = "one";
+//   heading1.style.color = "violet";
+//   setTimeout(() => {
+//     heading2.textContent = "two";
+//     heading2.style.color = "red";
+//     setTimeout(() => {
+//       heading3.textContent = "three";
+//       heading3.style.color = "purple";
+//       setTimeout(() => {
+//         heading4.textContent = "four";
+//         heading4.style.color = "pink";
+//         setTimeout(() => {
+//           heading5.textContent = "five";
+//           heading5.style.color = "green";
+//           setTimeout(() => {
+//             heading6.textContent = "six";
+//             heading6.style.color = "blue";
+//             setTimeout(() => {
+//               heading7.textContent = "seven";
+//               heading7.style.color = "brown";
+//             }, 1000);
+//           }, 3000);
+//         }, 2000);
+//       }, 1000);
+//     }, 2000);
+//   }, 2000);
+// }, 1000);
